859b633fdc45f85a30c644a4334ea3e4f9acc35f
Your Name <you@example.com>
2019-06-05 14:55:18 -0400

    btn state

--- feeder.ino
+++ feeder.ino
@@ -1,9 +1,12 @@
 const int minusBtn_pin = 2;
 const int setBtn_pin = 3;
+int setBtn_currState = LOW;
+int setBtn_prevState = LOW;
 const int plusBtn_pin = 4;
-int timeSelect = 0; // 0 unselect, 1 hours, 2 minutes
-int hours = 0;
-int minutes = 0;
+boolean HH_state = false;
+boolean mm_state = false;
+int HH_value = 0;
+int mm_value = 0;
 
 void setup()
 {
@@ -16,48 +19,26 @@ void setup()
 
 void loop()
 {
-  if (digitalRead(setBtn_pin) == HIGH)
+  setBtn_currState = digitalRead(setBtn_pin);
+  if (setBtn_currState != setBtn_prevState)
   {
-    if (timeSelect == 0)
+    if (setBtn_currState == LOW)
     {
-      timeSelect = 1;
-    }
-    else if (timeSelect == 1)
-    {
-      timeSelect = 2;
-    }
-    else
-    {
-      timeSelect = 0;
+      if (!HH_state)
+      {
+        HH_state = true;
+        mm_state = false;
+      }
+      else
+      {
+        HH_state = false;
+        mm_state = true;
+      }
     }
   }
+  setBtn_prevState = setBtn_currState;
 
-  if (digitalRead(minusBtn_pin) == HIGH)
-  {
-    if ((timeSelect == 1) && (hours > 0))
-    {
-      hours--;
-    }
-    else if ((timeSelect == 2) && (minutes > 0))
-    {
-      minutes--;
-    }
-  }
-
-  if (digitalRead(plusBtn_pin) == HIGH)
-  {
-    if (timeSelect == 1)
-    {
-      hours++;
-    }
-    else if (timeSelect == 2)
-    {
-      minutes++;
-    }
-  }
-
-  Serial.print(hours);
-  Serial.print(":");
-  Serial.println(minutes);
-  delay(1000);
+  Serial.print(HH_state);
+  Serial.print(" : ");
+  Serial.println(mm_state);
 }
