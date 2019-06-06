f2159e76648f7ff9c603b27add19ad37e184a281
naxisinc <33284023+naxisinc@users.noreply.github.com>
2019-06-05 21:27:27 -0400

    digital clock is ready

--- feeder.ino
+++ feeder.ino
@@ -1,12 +1,22 @@
 const int minusBtn_pin = 2;
+int minusBtn_currState = LOW;
+int minusBtn_prevState = LOW;
+
 const int setBtn_pin = 3;
 int setBtn_currState = LOW;
 int setBtn_prevState = LOW;
+
 const int plusBtn_pin = 4;
+int plusBtn_currState = LOW;
+int plusBtn_prevState = LOW;
+
 boolean HH_state = false;
 boolean mm_state = false;
-int HH_value = 0;
-int mm_value = 0;
+int HH_value = 23;
+int mm_value = 59;
+
+boolean timeSet = false;
+int ss_counter = 0;
 
 void setup()
 {
@@ -14,31 +24,96 @@ void setup()
   pinMode(plusBtn_pin, INPUT);
   pinMode(minusBtn_pin, INPUT);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop()
 {
+  // Select hours or minutes
   setBtn_currState = digitalRead(setBtn_pin);
   if (setBtn_currState != setBtn_prevState)
   {
     if (setBtn_currState == LOW)
     {
-      if (!HH_state)
+      if (!HH_state && !mm_state)
       {
         HH_state = true;
-        mm_state = false;
       }
-      else
+      else if (HH_state)
       {
         HH_state = false;
         mm_state = true;
+      }
+      else
+      {
+        HH_state = mm_state = false;
+        timeSet = true;
       }
     }
   }
   setBtn_prevState = setBtn_currState;
 
-  Serial.print(HH_state);
+  // Decrease Time
+  minusBtn_currState = digitalRead(minusBtn_pin);
+  if ((minusBtn_currState != minusBtn_prevState) && (HH_state || mm_state))
+  {
+    if (minusBtn_currState == LOW)
+    {
+      if (HH_state)
+      {
+        HH_value = (HH_value > 0) ? HH_value - 1 : 23;
+      }
+      if (mm_state)
+      {
+        mm_value = (mm_value > 0) ? mm_value - 1 : 59;
+      }
+    }
+  }
+  minusBtn_prevState = minusBtn_currState;
+
+  // Increase Time
+  plusBtn_currState = digitalRead(plusBtn_pin);
+  if ((plusBtn_currState != plusBtn_prevState) && (HH_state || mm_state))
+  {
+    if (plusBtn_currState == LOW)
+    {
+      if (HH_state)
+      {
+        HH_value = (HH_value < 23) ? HH_value + 1 : 0;
+      }
+      if (mm_state)
+      {
+        mm_value = (mm_value < 59) ? mm_value + 1 : 0;
+      }
+    }
+  }
+  plusBtn_prevState = plusBtn_currState;
+
+  // Digital Clock Running
+  if (timeSet)
+  {
+    delay(1000);
+
+    ss_counter++;
+    if (ss_counter > 59)
+    {
+      mm_value++;
+      if (mm_value > 59)
+      {
+        HH_value++;
+        if (HH_value > 23)
+        {
+          HH_value = 0;
+        }
+        mm_value = 0;
+      }
+      ss_counter = 0;
+    }
+  }
+
+  Serial.print(HH_value);
   Serial.print(" : ");
-  Serial.println(mm_state);
+  Serial.print(mm_value);
+  Serial.print(" : ");
+  Serial.println(ss_counter);
 }
