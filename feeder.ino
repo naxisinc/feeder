cce2da8d26ff1f49fa0a65a3772a7d6c22af7311
naxisinc <33284023+naxisinc@users.noreply.github.com>
2019-06-06 23:36:44 -0400

    blink

--- feeder.ino
+++ feeder.ino
@@ -1,119 +1,205 @@
-const int minusBtn_pin = 2;
-int minusBtn_currState = LOW;
-int minusBtn_prevState = LOW;
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-const int setBtn_pin = 3;
-int setBtn_currState = LOW;
-int setBtn_prevState = LOW;
+LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-const int plusBtn_pin = 4;
+const int modeBtn_pin = 2;
+int modeBtn_currState = LOW;
+int modeBtn_prevState = LOW;
+
+const int plusBtn_pin = 3;
 int plusBtn_currState = LOW;
 int plusBtn_prevState = LOW;
 
-boolean HH_state = false;
-boolean mm_state = false;
-int HH_value = 23;
-int mm_value = 59;
+const int setBtn_pin = 4;
+int setBtn_currState = LOW;
+int setBtn_prevState = LOW;
 
-boolean timeSet = false;
+int settings_arr[5][3] = {
+    {0, 0, 0},
+    {1, 1, 1},
+    {2, 2, 2},
+    {3, 3, 3},
+    {4, 4, 4}};
+int posPointer = -1;
+bool setFlag = false;
+
+bool HH_state = false;
+bool mm_state = false;
+bool feed_state = false;
+
 int ss_counter = 0;
 
 void setup()
 {
   pinMode(setBtn_pin, INPUT);
   pinMode(plusBtn_pin, INPUT);
-  pinMode(minusBtn_pin, INPUT);
+  pinMode(modeBtn_pin, INPUT);
 
   Serial.begin(115200);
+
+  // for (int i = 0; i < 5; i++)
+  // {
+  //   for (int j = 0; j < 3; j++)
+  //   {
+  //     Serial.print(settings_arr[i][j]);
+  //     Serial.print(" - ");
+  //   }
+  //   Serial.println();
+  // }
+
+  lcd.init(); // initialize the lcd
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Welcome Feeder!");
 }
 
 void loop()
 {
-  // Select hours or minutes
+  blink(11, 1);
+  modeBtn_currState = digitalRead(modeBtn_pin);
+  if (modeBtn_currState != modeBtn_prevState)
+  {
+    if (modeBtn_currState == LOW)
+    {
+      if (setFlag)
+      {
+        if (HH_state)
+        {
+          HH_state = false;
+          mm_state = true;
+        }
+        else if (mm_state)
+        {
+          mm_state = false;
+          feed_state = true;
+        }
+        else
+        {
+          HH_state = true;
+          mm_state = false;
+          feed_state = false;
+        }
+
+        HH_state = false;
+      }
+      else
+      {
+        posPointer = posPointer < 4 ? posPointer + 1 : 0;
+        lcdPrint();
+      }
+    }
+  }
+  modeBtn_prevState = modeBtn_currState;
+
+  // Set Time
   setBtn_currState = digitalRead(setBtn_pin);
   if (setBtn_currState != setBtn_prevState)
   {
     if (setBtn_currState == LOW)
     {
-      if (!HH_state && !mm_state)
+      setFlag = !setFlag;
+      if (setFlag)
       {
         HH_state = true;
-      }
-      else if (HH_state)
-      {
-        HH_state = false;
-        mm_state = true;
+        blink(11, 1);
+        // blink(12, 1);
       }
       else
       {
-        HH_state = mm_state = false;
-        timeSet = true;
+        HH_state = false;
+        mm_state = false;
+        feed_state = false;
       }
     }
   }
   setBtn_prevState = setBtn_currState;
 
-  // Decrease Time
-  minusBtn_currState = digitalRead(minusBtn_pin);
-  if ((minusBtn_currState != minusBtn_prevState) && (HH_state || mm_state))
-  {
-    if (minusBtn_currState == LOW)
-    {
-      if (HH_state)
-      {
-        HH_value = (HH_value > 0) ? HH_value - 1 : 23;
-      }
-      if (mm_state)
-      {
-        mm_value = (mm_value > 0) ? mm_value - 1 : 59;
-      }
-    }
-  }
-  minusBtn_prevState = minusBtn_currState;
-
   // Increase Time
   plusBtn_currState = digitalRead(plusBtn_pin);
-  if ((plusBtn_currState != plusBtn_prevState) && (HH_state || mm_state))
+  if ((plusBtn_currState != plusBtn_prevState) && (HH_state || mm_state || feed_state))
   {
     if (plusBtn_currState == LOW)
     {
       if (HH_state)
       {
-        HH_value = (HH_value < 23) ? HH_value + 1 : 0;
+        settings_arr[posPointer][0] = (settings_arr[posPointer][0] < 23) ? settings_arr[posPointer][0] + 1 : 0;
+        lcdPrint();
       }
-      if (mm_state)
+      else if (mm_state)
       {
-        mm_value = (mm_value < 59) ? mm_value + 1 : 0;
+        settings_arr[posPointer][1] = (settings_arr[posPointer][1] < 59) ? settings_arr[posPointer][1] + 1 : 0;
+        lcdPrint();
+      }
+      else
+      {
+        settings_arr[posPointer][2] = (settings_arr[posPointer][2] < 2) ? settings_arr[posPointer][2] + 1 : 0;
+        lcdPrint();
       }
     }
   }
   plusBtn_prevState = plusBtn_currState;
 
   // Digital Clock Running
-  if (timeSet)
-  {
-    delay(1000);
+  /*delay(1000);
 
-    ss_counter++;
-    if (ss_counter > 59)
+  ss_counter++;
+  if (ss_counter > 59)
+  {
+    settings_arr[0][1]++; // minutes
+    if (settings_arr[0][1] > 59)
     {
-      mm_value++;
-      if (mm_value > 59)
+      settings_arr[0][0]++; // hours
+      if (settings_arr[0][0] > 23)
       {
-        HH_value++;
-        if (HH_value > 23)
-        {
-          HH_value = 0;
-        }
-        mm_value = 0;
+        settings_arr[0][0] = 0;
       }
-      ss_counter = 0;
+      settings_arr[0][1] = 0;
     }
+    ss_counter = 0;
+  }*/
+}
+
+void lcdPrint()
+{
+  lcd.clear();
+  if (posPointer != 0)
+  {
+    lcd.setCursor(15, 0);
+    lcd.print(settings_arr[posPointer][2]);
   }
 
-  Serial.print(HH_value);
-  Serial.print(" : ");
-  Serial.print(mm_value);
-  Serial.print(" : ");
-  Serial.println(ss_counter);
+  if (settings_arr[posPointer][0] < 10)
+  {
+    lcd.setCursor(12, 1);
+  }
+  else
+  {
+    lcd.setCursor(11, 1);
+  }
+  lcd.print(settings_arr[posPointer][0]);
+  lcd.print(":");
+
+  if (settings_arr[posPointer][1] < 10)
+  {
+    lcd.print("0");
+  }
+  lcd.print(settings_arr[posPointer][1]);
+}
+
+bool turnOff = false;
+void blink(int col, int row)
+{
+  delay(500);
+  lcd.setCursor(11, 1);
+  if (turnOff)
+  {
+    lcd.print(" ");
+  }
+  else
+  {
+    lcd.print("2");
+  }
+  turnOff = !turnOff;
 }
